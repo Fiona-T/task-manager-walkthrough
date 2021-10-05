226ccac1a64fb65377889dab383859bcb6a8ac8d
Fiona-T <fitracey@gmail.com>
2021-10-05 08:34:42 +0000

    update categories route in routes.py to retrieve category names, update categories.html to display names in card

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -11,29 +11,33 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    # query the Category model, sort by name, cast to list for use in template
+    # as .all() returns a cursor object
+    categories = list(Category.query.order_by(Category.category_name).all())
+    # pass categories list to template, using variable name categories
+    return render_template("categories.html", categories=categories)
 
 
 # include the methods here as we submit a form on this page with POST
-# @app.route("/add_category", methods=["GET", "POST"])
-# def add_category():
-#     # if the request method is POST, then new variable category=new instance
-#     # of Category model
-#     if request.method == "POST":
-#         category = Category(category_name=request.form.get("category_name"))
-#         # add + commit the new category to SQLAlchemy database variable of db (imported)
-#         # uses sessionmaker instance
-#         db.session.add(category)
-#         db.session.commit()
-#         # redirect the page to the categories page after form submitted
-#         return redirect(url_for("categories"))
-#     return render_template("add_category.html")
-
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    # if the request method is POST, then new variable category=new instance
+    # of Category model
     if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
+        # add + commit the new category to SQLAlchemy database variable of
+        # db (imported). Uses sessionmaker instance
         db.session.add(category)
         db.session.commit()
+        # redirect the page to the categories page after form submitted
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+# @app.route("/add_category", methods=["GET", "POST"])
+# def add_category():
+#     if request.method == "POST":
+#         category = Category(category_name=request.form.get("category_name"))
+#         db.session.add(category)
+#         db.session.commit()
+#         return redirect(url_for("categories"))
+#     return render_template("add_category.html")
