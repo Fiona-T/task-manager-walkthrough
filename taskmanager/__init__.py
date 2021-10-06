2f4211737080d7ba87dba30503b6388f4ad45982
Fiona-T <fitracey@gmail.com>
2021-10-06 10:21:31 +0000

    add conditional check in __init__.py file to ensure correct database path is used

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -13,7 +13,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 # config variables, set to get their respective environment variable
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+# for database url config variable, need to specify which url path to use
+# there are two versions of db, one local and one in Heroku
+# if env variable DEVELOPMENT is set to true, use the local database path DB_URL
+# otherwise (as this is not set in Heroku), use Heroku path DATABASE_URL
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 # create instance of imported SQLAlchemy class, set to our instance of Flask
 db = SQLAlchemy(app)
